e73d0a1e454e45604c0c7498db322cddd42547ed
bouncey <staylor1012@gmail.com>
2016-07-30 00:03:57 +0100

    Commit before deployment

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 8000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+function getMilSec(time) {
+  var mills = time.getTime();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  if(mills){
+    return mills;
+  }else{
+    return null;
+  }
+}
+function getNormTime(time) {
+  var year = time.getFullYear(),
+      month = time.getMonth(),
+      day = time.getDate(),
+      monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"],
+      longMonth = monthNames[month];
+
+  if(day && longMonth && year){
+    return `"${day} ${longMonth} ${year}"`;
+  }else{
+    return null;
+  }
+}
+app.get('*', ((req, res) => {
+    var request = new Date(req.originalUrl.replace(/^\//, ''));
+    res.send(`{ "unix": ${getMilSec(request)}, "natural": ${getNormTime(request)} }`);
+}));
+
+app.listen(port);
+console.log('server is running...');
